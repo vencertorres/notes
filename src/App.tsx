c35289bacce74b78a2d5ccfe6b0573690d066485
Vencer Torres <vencer.torres@gmail.com>
2023-03-07 17:14:36 +0800

    feat: add create note function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns"
-import { Route } from "wouter"
+import { Route, useLocation } from "wouter"
+import MaterialSymbolsAddNotesRounded from "~icons/material-symbols/add-notes-rounded"
 import NoteLink from "./components/NoteLink"
 import NoteView from "./components/NoteView"
 import { useLocalStorage } from "./hooks"
@@ -7,10 +8,31 @@ import { Note } from "./types"
 
 function App() {
   const [notes, setNotes] = useLocalStorage<Note[]>("notes", [])
+  const [location, setLocation] = useLocation()
+
+  function createNote() {
+    const note = {
+      id: Math.random().toString(36).slice(2, 7),
+      title: "",
+      body: "",
+      time: new Date().toJSON()
+    }
+    setNotes(notes.concat(note))
+    setLocation(`/notes/${note.id}`)
+  }
 
   return (
     <div className="sm:grid grid-cols-[20rem,_1fr] h-screen overflow-y-auto">
-      <div className="sm:w-80 bg-gray-50 border-r overflow-y-auto">
+      <div className="sm:w-80 bg-white border-r overflow-y-auto">
+        <div className="flex gap-4 p-4 border-b">
+          <input
+            type="search"
+            placeholder="Search note"
+            className="w-full px-4 py-2 mr-auto border rounded-full bg-gray-100 outline-none" />
+          <button title="Create note" onClick={createNote} className="text-blue-500 hover:text-blue-700">
+            <MaterialSymbolsAddNotesRounded className="w-6 h-6" />
+          </button>
+        </div>
         {notes.length ? (
           <ul>
             {notes.map(note => (
