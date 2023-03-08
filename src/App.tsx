56f0337f38e89efa37336ca0851851791bbdca4c
Vencer Torres <vencer.torres@gmail.com>
2023-03-08 12:58:37 +0800

    feat: add search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns"
+import { useState } from "react"
 import { useLocation } from "wouter"
 import MaterialSymbolsAddNotesRounded from "~icons/material-symbols/add-notes-rounded"
 import NoteLink from "./components/NoteLink"
@@ -9,6 +10,9 @@ import { Note } from "./types"
 function App() {
   const [notes, setNotes] = useLocalStorage<Note[]>("notes", [])
   const [location, setLocation] = useLocation()
+  const [query, setQuery] = useState(() => {
+    return new URLSearchParams(window.location.search).get("q") ?? ""
+  })
 
   function createNote() {
     const note = {
@@ -21,6 +25,21 @@ function App() {
     setLocation(`/notes/${note.id}`)
   }
 
+  function search(event: React.ChangeEvent<HTMLInputElement>) {
+    setLocation(event.target.value ? `/?q=${event.target.value}` : "/", { replace: true })
+    setQuery(event.target.value)
+  }
+
+  let list = query
+    ? notes.filter(note => {
+        if  (note.title.toLowerCase().includes(query.toLowerCase())
+            || note.body.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return true
+            }
+      })
+    : notes
+
   return (
     <div className="sm:grid grid-cols-[20rem,_1fr] h-screen overflow-y-auto">
       <div className="flex flex-col sm:w-80 bg-white border-r overflow-y-auto">
@@ -29,14 +48,16 @@ function App() {
             type="search"
             placeholder="Search note"
             className="w-full px-4 py-2 mr-auto border rounded-full bg-gray-100 outline-none"
+            value={query}
+            onChange={search}
           />
           <button title="Create note" onClick={createNote} className="text-blue-500 hover:text-blue-700">
             <MaterialSymbolsAddNotesRounded className="w-6 h-6" />
           </button>
         </div>
-        {notes.length ? (
+        {list.length ? (
           <ul>
-            {notes.map(note => (
+            {list.map(note => (
               <li key={note.id}>
                 <NoteLink href={`/notes/${note.id}`}>
                   <div className="p-4 border-b bg-white group-hover:bg-gray-100 group-[.active]:bg-blue-50">
